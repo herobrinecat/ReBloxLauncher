fd3e8e4379718c989be36b6bb38cd13e15e95cec
herobrinecat <89881391+herobrinecat@users.noreply.github.com>
2026-02-28 02:18:19 -0600

    Remove unused using directive from LaunchScreen.cs

--- ReBloxLauncher/LaunchScreen.cs
+++ ReBloxLauncher/LaunchScreen.cs
@@ -14,7 +14,6 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace ReBloxLauncher
 {
