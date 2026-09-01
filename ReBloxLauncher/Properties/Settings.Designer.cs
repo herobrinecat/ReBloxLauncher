eddfd7e6d97fc69ab8f995104080ed94dcab1ca4
herobrinecat <89881391+herobrinecat@users.noreply.github.com>
2026-09-01 18:18:32 -0500

    Update DefaultSettingValue for UpdateURL

--- ReBloxLauncher/Properties/Settings.Designer.cs
+++ ReBloxLauncher/Properties/Settings.Designer.cs
@@ -360,7 +360,7 @@ namespace ReBloxLauncher.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("http://rebloxfileserver.servehttp.com:81")]
+        [global::System.Configuration.DefaultSettingValueAttribute("http://rebloxfileserver.servehttp.com")]
         public string UpdateURL {
             get {
                 return ((string)(this["UpdateURL"]));
